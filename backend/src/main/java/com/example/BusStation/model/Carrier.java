e7c264715599c9debf976e49ace0db7e547e077b
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-19 11:55:35 +0100

    Created carrier entity

---
+++ backend/src/main/java/com/example/BusStation/model/Carrier.java
@@ -0,0 +1,37 @@
+package com.example.BusStation.model;
+
+import com.example.BusStation.config.SecurityConfig;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Carrier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true)
+    private String eMail;
+
+    @OneToMany(mappedBy = "carrier")
+    private Set<Bus> buses;
+
+    @ManyToMany(mappedBy = "carriers")
+    private Set<Line> lines;
+
+}
