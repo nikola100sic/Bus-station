ba77ee9ef51ac6cefdbd650d7894b5b40715dcff
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-20 11:51:10 +0100

    Added CRUD methods in reservation service

--- backend/src/main/java/com/example/BusStation/service/ReservationService.java
+++ backend/src/main/java/com/example/BusStation/service/ReservationService.java
@@ -1,4 +1,18 @@
 package com.example.BusStation.service;
 
+import com.example.BusStation.model.Reservation;
+
+import java.util.List;
+
 public interface ReservationService {
+
+    Reservation getOne(Long id);
+
+    List<Reservation> getAll();
+
+    Reservation save(Reservation line);
+
+    Reservation update(Reservation reservation);
+
+    Reservation delete(Long id);
 }
