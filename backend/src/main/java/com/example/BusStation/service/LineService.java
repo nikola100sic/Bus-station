75328fb327b56fcc490a37999f74dd07706d8d6c
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-20 11:50:58 +0100

    Added CRUD methods in line service

--- backend/src/main/java/com/example/BusStation/service/LineService.java
+++ backend/src/main/java/com/example/BusStation/service/LineService.java
@@ -1,4 +1,18 @@
 package com.example.BusStation.service;
 
+import com.example.BusStation.model.Line;
+
+import java.util.List;
+
 public interface LineService {
+
+    Line getOne(Long id);
+
+    List<Line> getAll();
+
+    Line save(Line line);
+
+    Line update(Line line);
+
+    Line delete(Long id);
 }
