e67dfad77ebc90dcfb7e0164dc0c17633ec0518a
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-20 11:50:12 +0100

    Added CRUD methods in bus service

--- backend/src/main/java/com/example/BusStation/service/BusService.java
+++ backend/src/main/java/com/example/BusStation/service/BusService.java
@@ -1,4 +1,19 @@
 package com.example.BusStation.service;
 
+import com.example.BusStation.model.Bus;
+
+import java.util.List;
+
 public interface BusService {
+
+    Bus getOne(Long id);
+
+    List<Bus> getAll();
+
+    Bus save(Bus bus);
+
+    Bus update(Bus bus);
+
+    Bus delete(Long id);
+
 }
