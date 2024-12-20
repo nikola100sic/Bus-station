373a7faa25db83dbf11f166d6d5f48ba2498b940
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-20 11:50:28 +0100

    Added CRUD methods in carrier service

--- backend/src/main/java/com/example/BusStation/service/CarrierService.java
+++ backend/src/main/java/com/example/BusStation/service/CarrierService.java
@@ -1,4 +1,18 @@
 package com.example.BusStation.service;
 
+import com.example.BusStation.model.Carrier;
+
+import java.util.List;
+
 public interface CarrierService {
+
+    Carrier getOne(Long id);
+
+    List<Carrier> getAll();
+
+    Carrier save(Carrier carrier);
+
+    Carrier update(Carrier carrier);
+
+    Carrier delete(Long id);
 }
