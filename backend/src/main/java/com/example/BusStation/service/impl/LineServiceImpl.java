547768c33364d583fe9676b208f32f0c549e0f51
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-21 22:12:57 +0100

    Updated line service implementation

--- backend/src/main/java/com/example/BusStation/service/impl/LineServiceImpl.java
+++ backend/src/main/java/com/example/BusStation/service/impl/LineServiceImpl.java
@@ -1,35 +1,55 @@
 package com.example.BusStation.service.impl;
 
+import com.example.BusStation.model.Carrier;
 import com.example.BusStation.model.Line;
+import com.example.BusStation.repository.LineRepository;
 import com.example.BusStation.service.LineService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class LineServiceImpl implements LineService {
+
+    private final LineRepository lineRepository;
+
     @Override
     public Line getOne(Long id) {
-        return null;
+        return lineRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Entity with ID " + id + " not found"));
     }
 
     @Override
     public List<Line> getAll() {
-        return List.of();
+        return lineRepository.findAll();
     }
 
     @Override
     public Line save(Line line) {
-        return null;
+        return lineRepository.save(line);
     }
 
     @Override
     public Line update(Line line) {
-        return null;
+        if (line == null || line.getId() == null) {
+            throw new IllegalArgumentException("Line or Line ID must not be null.");
+        }
+        Line existingLine = lineRepository.findById(line.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Line with ID " + line.getId() + " not found."));
+        return lineRepository.save(line);
     }
 
     @Override
     public Line delete(Long id) {
-        return null;
+        if (id == null) {
+            throw new IllegalArgumentException("ID must not be null.");
+        }
+        Line line = lineRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Line with ID: " + id + " not found"));
+        lineRepository.delete(line);
+        return line;
     }
 }
