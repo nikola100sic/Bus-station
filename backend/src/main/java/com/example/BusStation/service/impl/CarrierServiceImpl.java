cdb8480b300fb914a979d0b6e6ee6e293600150d
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-21 21:58:31 +0100

    Updated carrier service implementation

--- backend/src/main/java/com/example/BusStation/service/impl/CarrierServiceImpl.java
+++ backend/src/main/java/com/example/BusStation/service/impl/CarrierServiceImpl.java
@@ -1,35 +1,53 @@
 package com.example.BusStation.service.impl;
 
 import com.example.BusStation.model.Carrier;
+import com.example.BusStation.repository.CarrierRepository;
 import com.example.BusStation.service.CarrierService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class CarrierServiceImpl implements CarrierService {
+
+    private final CarrierRepository carrierRepository;
+
     @Override
     public Carrier getOne(Long id) {
-        return null;
+        return carrierRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Carrier with ID: " + id + " not found"));
     }
 
     @Override
     public List<Carrier> getAll() {
-        return List.of();
+        return carrierRepository.findAll();
     }
 
     @Override
     public Carrier save(Carrier carrier) {
-        return null;
+        return carrierRepository.save(carrier);
     }
 
     @Override
     public Carrier update(Carrier carrier) {
-        return null;
+        if (carrier == null || carrier.getId() == null) {
+            throw new IllegalArgumentException("Carrier or Carrier ID must not be null.");
+        }
+        Carrier existingCarrier = carrierRepository.findById(carrier.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Carrier with ID " + carrier.getId() + " not found."));
+        return carrierRepository.save(carrier);
     }
 
     @Override
     public Carrier delete(Long id) {
-        return null;
+        if (id == null) {
+            throw new IllegalArgumentException("ID must not be null.");
+        }
+        Carrier carrier = carrierRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Carrier with ID: " + id + " not found"));
+        carrierRepository.delete(carrier);
+        return carrier;
     }
 }
