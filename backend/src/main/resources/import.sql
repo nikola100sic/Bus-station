a021e3d78ef0387a3fed2423bf15cf9a9ec991df
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-27 13:42:58 +0100

    Added users in test data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,7 @@
+INSERT INTO `busstation`.`user` (`id`, `name`,`surname`,`e_mail`,`password`, `username`, `user_role`, `is_email_verified`, `token`) VALUES ('1','Nikola','Stosic','nikola@nikola' ,'$2a$10$leAXJYphj5ZuzUeI8/xF4Oe6sW7D/u8KlbVGgLFw6BkXxjrzlYWeq', 'dzoni', 'ADMIN',1, '1233');
+INSERT INTO `busstation`.`user` (`id`, `name`,`surname`,`e_mail`,`password`, `username`, `user_role`, `is_email_verified`, `token`) VALUES ('2','Petar','Stosic','petar@petar' ,'$2a$10$rGFzFTHsScJ4X8SU5/9CweD07SeX67/GxRUHWnKLnUEaUPx/rgOsC', 'rope', 'USER', 1, '123');
+
+
 INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_number`) VALUES ('1', 'Njegoseva 22', 'nstravel@gmail.com', 'NS Travel', '021 345 233');
 INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_number`) VALUES ('2', 'Njegoseva 28', 'nikolaTurs@gmail.com', 'Nikola turs', '018 345 443');
 INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_number`) VALUES ('3', 'Dositejeva 72', 'globus@gmail.com', 'Globus Travel', '011 673 233');
