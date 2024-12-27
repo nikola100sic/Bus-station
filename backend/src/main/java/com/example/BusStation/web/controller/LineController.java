9a9ecfae2efc1d77ecb1254b987ce6a38107526b
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-27 13:41:54 +0100

    Search method used

--- backend/src/main/java/com/example/BusStation/web/controller/LineController.java
+++ backend/src/main/java/com/example/BusStation/web/controller/LineController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,24 +19,31 @@ public class LineController {
 
     private final LineService lineService;
 
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     @GetMapping
-    public ResponseEntity<List<LineDTO>> getAll() {
-        List<LineDTO> lines = lineService.getAll();
+    public ResponseEntity<List<LineDTO>> getAll(@RequestParam(required = false) Long carrierId,
+                                                @RequestParam(required = false) String destination,
+                                                @RequestParam(required = false) Integer maxPrice,
+                                                @RequestParam(value = "pageNo", defaultValue = "0") int pageNo) {
+        List<LineDTO> lines = lineService.search(carrierId, destination, maxPrice, pageNo);
         return new ResponseEntity<>(lines, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     @GetMapping("/{id}")
     public ResponseEntity<LineDTO> getOne(@PathVariable Long id) {
         LineDTO lineDTO = lineService.getOne(id);
         return new ResponseEntity<>(lineDTO, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public ResponseEntity<LineDTO> save(@RequestBody Line line) {
         LineDTO savedLine = lineService.save(line);
         return new ResponseEntity<>(savedLine, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<LineDTO> update(@PathVariable Long id, @RequestBody Line line) {
         LineDTO updated = lineService.update(line, id);
@@ -43,6 +51,7 @@ public class LineController {
 
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         lineService.delete(id);
