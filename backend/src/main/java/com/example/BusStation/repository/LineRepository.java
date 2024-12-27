2d1b42a4fbfbf9285c21ea108f0ac345d8b44ebd
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-27 13:34:54 +0100

    Added query for search

--- backend/src/main/java/com/example/BusStation/repository/LineRepository.java
+++ backend/src/main/java/com/example/BusStation/repository/LineRepository.java
@@ -1,7 +1,21 @@
 package com.example.BusStation.repository;
 
 import com.example.BusStation.model.Line;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
+
+    @Query("SELECT l FROM Line l JOIN l.carriers c " +
+            "WHERE (:carrierId IS NULL OR c.id = :carrierId) " +
+            "AND (:destination IS NULL OR LOWER(l.destination) LIKE LOWER(CONCAT('%', :destination, '%'))) " +
+            "AND (:maxPrice IS NULL OR l.price <= :maxPrice)")
+    List<Line> searchLines(@Param("carrierId") Long carrierId,
+                           @Param("destination") String destination,
+                           @Param("maxPrice") Integer maxPrice, Pageable pageable);
+
 }
