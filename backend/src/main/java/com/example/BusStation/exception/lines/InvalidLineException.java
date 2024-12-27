6f8e5d6fc299a261a3742eadec60e8a3dc14bcf2
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-27 10:46:01 +0100

    Updated invalid line exception

--- backend/src/main/java/com/example/BusStation/exception/lines/InvalidLineException.java
+++ backend/src/main/java/com/example/BusStation/exception/lines/InvalidLineException.java
@@ -1,10 +1,7 @@
 package com.example.BusStation.exception.lines;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class InvalidLineException extends RuntimeException {
-    
+
     public InvalidLineException(String message) {
         super(message);
     }
