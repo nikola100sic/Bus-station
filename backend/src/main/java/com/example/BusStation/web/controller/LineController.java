b2b2a04af2159791a1da769c08c232a89c3cb0f5
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-27 10:30:33 +0100

    Updated line controller

--- backend/src/main/java/com/example/BusStation/web/controller/LineController.java
+++ backend/src/main/java/com/example/BusStation/web/controller/LineController.java
@@ -3,12 +3,10 @@ package com.example.BusStation.web.controller;
 import com.example.BusStation.model.Line;
 import com.example.BusStation.service.LineService;
 import com.example.BusStation.web.dto.LineDTO;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,9 +24,28 @@ public class LineController {
         return new ResponseEntity<>(lines, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<LineDTO> getOne(@PathVariable Long id) {
+        LineDTO lineDTO = lineService.getOne(id);
+        return new ResponseEntity<>(lineDTO, HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<LineDTO> save(@RequestBody Line line) {
         LineDTO savedLine = lineService.save(line);
         return new ResponseEntity<>(savedLine, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LineDTO> update(@PathVariable Long id, @RequestBody Line line) {
+        LineDTO updated = lineService.update(line, id);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        lineService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
