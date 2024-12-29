3852abfb6cdf308f548406784b48ac592bcb1d42
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-29 20:26:30 +0100

    Updated footer component

---
+++ frontend/src/components/shared/footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledFooter } from "./Footer.styled";
+
+const Footer = ({ text }) => {
+  return <StyledFooter>{text}</StyledFooter>;
+};
+
+export default Footer;
