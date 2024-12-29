ce1cf5f07a11dd21e875710ec643ebbdd30aba14
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-29 21:16:15 +0100

    Added styles for header

---
+++ frontend/src/components/shared/header/Header.styled.js
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  background: #076cab;
+  color: black;
+  box-shadow: 0px 10px 24px 0px rgba(0, 0, 0, 0.25);
+  opacity: 0.85;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 10;
+`;
+
+export const StyledLinks = styled(NavLink)`
+  text-decoration: none;
+  color: white;
+`;
+
+export const StyledLinksWrapper = styled.div`
+  display: flex;
+  gap: 100px;
+  flex-wrap: wrap;
+`;
+
+export const StyledRegistrationLoginWrapper = styled.div`
+  display: flex;
+  gap: 13px;
+  flex-direction: column;
+`;
+
+export const StyledLogoutWrapper = styled.div`
+  display: flex;
+  align-items: "center";
+  gap: 5px;
+`;
