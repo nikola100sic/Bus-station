570712e1255092e03770b9c3375bf9e96c42d81d
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-29 21:16:04 +0100

    Updated header component

--- frontend/src/components/shared/header/Header.js
+++ frontend/src/components/shared/header/Header.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import {
+  StyledHeader,
+  StyledLinks,
+  StyledLinksWrapper,
+  StyledLogoutWrapper,
+  StyledRegistrationLoginWrapper,
+} from "./Header.styled";
+import { Link } from "react-router-dom";
+import { FaUser, FaUserLarge, FaUserPlus } from "react-icons/fa6";
 
-const Header = () => {
-  return <div></div>;
+const Header = (brandName, navItems) => {
+  const token = localStorage.getItem("jwt");
+
+  const handleLogout = () => {
+    localStorage.removeItem("jwt");
+    window.location.replace("/home");
+  };
+
+  useEffect(() => {
+    if (token) {
+      try {
+        const decoded = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+        if (decoded.exp < currentTime) {
+          handleLogout();
+        }
+      } catch (error) {
+        console.error("Invalid token", error);
+        handleLogout();
+      }
+    }
+  }, [token]);
+  return (
+    <StyledHeader>
+      <StyledLinks to={"/home"}>
+        <h3>{brandName}</h3>
+      </StyledLinks>
+      <StyledLinksWrapper>
+        {navItems.map((item) => (
+          <StyledLinks to={item.path} key={item.path}>
+            {item.title}
+          </StyledLinks>
+        ))}
+      </StyledLinksWrapper>
+      <StyledRegistrationLoginWrapper>
+        {token ? (
+          //TODO ADD!
+          <></>
+        ) : (
+          <>
+            <StyledLinks to={"/login"}>
+              <FaUser style={{ marginRight: "5px" }} /> Login
+            </StyledLinks>
+            <StyledLinks to={"/registration"}>
+              <FaUserPlus style={{ marginRight: "5px" }} /> Registration
+            </StyledLinks>
+          </>
+        )}
+      </StyledRegistrationLoginWrapper>
+    </StyledHeader>
+  );
 };
 
 export default Header;
