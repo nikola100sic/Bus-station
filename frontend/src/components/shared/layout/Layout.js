10cf5c40ff8a29c455db17b38baf77b48c89bcd6
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-30 17:14:32 +0100

    Updated layout

--- frontend/src/components/shared/layout/Layout.js
+++ frontend/src/components/shared/layout/Layout.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Content, StyledPageContainer } from "./Layout.styled";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
 
-const Layout = () => {
-  return <div></div>;
+const NAVITEMS = [
+  {
+    title: "Home",
+    path: "/home",
+    requiresAuth: false,
+  },
+  {
+    title: "Lines",
+    path: "/lines",
+    requiresAuth: false,
+  },
+];
+const Layout = ({ children }) => {
+  const token = localStorage.getItem("jwt");
+  const filteredNavItems = NAVITEMS.filter((item) =>
+    item.requiresAuth ? token : true
+  );
+
+  return (
+    <StyledPageContainer>
+      <Header brandName="Bus station" navItems={filteredNavItems}></Header>
+      <Content>{children}</Content>
+      <Footer text="© 2025 Bus station. All rights reserved. | Made by Nikola"></Footer>
+    </StyledPageContainer>
+  );
 };
 
 export default Layout;
