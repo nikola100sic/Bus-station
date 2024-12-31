4edd63c4221e3bc3f966f6f7c42f44fb9d4853fa
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-31 12:10:25 +0100

    Updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/shared/layout/Layout";
+import Home from "./pages/home/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Layout>
+    </>
+  );
 }
 
 export default App;
