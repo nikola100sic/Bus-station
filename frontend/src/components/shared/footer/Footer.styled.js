c3717f215c86102a9821aba9d974b864ca7dc9e2
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-31 12:09:51 +0100

    Updated footer styles

--- frontend/src/components/shared/footer/Footer.styled.js
+++ frontend/src/components/shared/footer/Footer.styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
-  background: burlywood;
+  background: #e48306a3;
   color: azure;
   padding: 20px;
   text-align: center;
