4e2e9a66af36491ac546d9e2afcc913cf9eb56db
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-01 19:07:00 +0100

    Updated axios

---
+++ frontend/src/components/api/axios.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const CinemaAxios = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+export const AuthAxios = axios.create({
+  baseURL: process.env.REACT_APP_AUTH_URL,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+CinemaAxios.interceptors.request.use(function add_jwt(config) {
+  if (window.localStorage["jwt"]) {
+    config.headers["Authorization"] = "Bearer " + window.localStorage["jwt"];
+  }
+
+  return config;
+});
