7bf6f90733bc5f55521dcef8e043f402a7db06cf
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-06 15:25:45 +0100

    Updated line service

--- frontend/src/components/api/line.service.axios.js
+++ frontend/src/components/api/line.service.axios.js
@@ -10,7 +10,22 @@ const getLine = (id) => {
   return BusStationAxios.get(`/lines/${id}`);
 };
 
+const deleteLine = (id) => {
+  return BusStationAxios.delete(`/lines/${id}`);
+};
+
+const updateLine = (line) => {
+  return BusStationAxios.update(`/lines`, line);
+};
+
+const addLine = (newLine) => {
+  return BusStationAxios.add(`/lines`, newLine);
+};
+
 export default {
   getLines,
   getLine,
+  deleteLine,
+  updateLine,
+  addLine,
 };
