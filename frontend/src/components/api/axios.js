dc9250ca2856b9b281719113b433b12941a4476b
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-06 15:16:37 +0100

    Updated axios.js

--- frontend/src/components/api/axios.js
+++ frontend/src/components/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const CinemaAxios = axios.create({
+export const BusStationAxios = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
     Accept: "application/json",
@@ -14,7 +14,7 @@ export const AuthAxios = axios.create({
   },
 });
 
-CinemaAxios.interceptors.request.use(function add_jwt(config) {
+BusStationAxios.interceptors.request.use(function add_jwt(config) {
   if (window.localStorage["jwt"]) {
     config.headers["Authorization"] = "Bearer " + window.localStorage["jwt"];
   }
