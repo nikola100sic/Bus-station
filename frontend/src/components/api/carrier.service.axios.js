5590f942e92ff98e9e9e639e630f64dc821bfeba
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-06 21:18:38 +0100

    Updated carrier service axios

--- frontend/src/components/api/carrier.service.axios.js
+++ frontend/src/components/api/carrier.service.axios.js
@@ -4,6 +4,16 @@ const getCarrier = (id) => {
   return BusStationAxios.get(`/carriers/${id}`);
 };
 
+const getCarriers = () => {
+  return BusStationAxios.get(`/carriers`);
+};
+
+const addCarier = (newCarrier) => {
+  return BusStationAxios.add(`/carriers`, newCarrier);
+};
+
 export default {
   getCarrier,
+  getCarriers,
+  addCarier,
 };
