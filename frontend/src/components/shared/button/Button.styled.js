d30bc4fded06e279aea564b7e26beee381f70ef2
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-07 17:59:27 +0100

    Updated button styled

---
+++ frontend/src/components/shared/button/Button.styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  width: 110px;
+  height: 40px;
+  background: ${(props) =>
+    props.$color
+      ? props.$color
+      : props.$variant === "filled"
+      ? "red"
+      : "transparent"};
+  border: ${(props) => (props.$variant === "filled" ? "none" : "#ff000094")};
+  color: ${(props) => props.$textColor || "black"};
+  &:hover {
+    cursor: pointer;
+  }
+  border-radius: 15px;
+`;
+
+export const SubmitButton = styled(StyledButton)`
+  background-color: #0014ff66;
+  color: black;
+  border: none;
+  display: block;
+  margin: 0;
+`;
