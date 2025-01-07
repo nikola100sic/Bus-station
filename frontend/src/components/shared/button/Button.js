81556f752a1c23c3763b10368f66a19bab4b8778
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-07 17:59:16 +0100

    Updated button

--- frontend/src/components/shared/button/Button.js
+++ frontend/src/components/shared/button/Button.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { StyledButton, SubmitButton } from "./Button.styled";
 
-const Button = () => {
-  return <div></div>;
+const Button = (text, onClick, variant, type, color, textColor, disabled) => {
+  return (
+    <>
+      {type === "submit" ? (
+        <SubmitButton
+          $variant={variant}
+          $color={color}
+          $textcolor={textColor}
+          disabled={disabled}
+          type={type}
+          onClick={onClick}
+        >
+          {text}
+        </SubmitButton>
+      ) : (
+        <StyledButton
+          $variant={variant}
+          $color={color}
+          $textcolor={textColor}
+          disabled={disabled}
+          type={type}
+          onClick={onClick}
+        >
+          {text}
+        </StyledButton>
+      )}
+    </>
+  );
 };
 
 export default Button;
