92ec05ca4a840a007b70498bd80933e75b94ebc1
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-11 23:20:56 +0100

    Updated Line page

---
+++ frontend/src/pages/lines/LinePage.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const LinePage = () => {
+  const [lines, setLines] = useState(null);
+  return (
+    <div>
+      <p>Lines</p>
+    </div>
+  );
+};
+
+export default LinePage;
