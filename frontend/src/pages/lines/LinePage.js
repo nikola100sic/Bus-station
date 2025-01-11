46cc937c60bc9d9790f8f18274effff0759bd445
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-11 23:45:49 +0100

    Updated line page

--- frontend/src/pages/lines/LinePage.js
+++ frontend/src/pages/lines/LinePage.js
@@ -1,7 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { BusStationAxios } from "../../components/api/axios";
+import lineServiceAxios from "../../components/api/line.service.axios";
 
 const LinePage = () => {
   const [lines, setLines] = useState(null);
+
+  const getLines = async (destination, carrierId, maxPrice) => {
+    try {
+      const res = await lineServiceAxios.getLines(
+        destination,
+        carrierId,
+        maxPrice
+      );
+      setLines(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error("Greška pri učitavanju linija:", error);
+    }
+  };
+
+  useEffect(() => {
+    getLines();
+  }, []);
+
   return (
     <div>
       <p>Lines</p>
