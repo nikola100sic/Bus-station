6cdd7860c62164f6845bd5793744de76c1686150
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-13 18:57:45 +0100

    Added departure param

--- backend/src/main/java/com/example/BusStation/repository/LineRepository.java
+++ backend/src/main/java/com/example/BusStation/repository/LineRepository.java
@@ -13,10 +13,12 @@ public interface LineRepository extends JpaRepository<Line, Long> {
     @Query("SELECT l FROM Line l JOIN l.carriers c " +
             "WHERE (:carrierId IS NULL OR c.id = :carrierId) " +
             "AND (:destination IS NULL OR LOWER(l.destination) LIKE LOWER(CONCAT('%', :destination, '%'))) " +
+            "AND (:departure IS NULL OR LOWER(l.departure) LIKE LOWER(CONCAT('%', :departure, '%'))) " +
             "AND (:maxPrice IS NULL OR l.price <= :maxPrice)" +
-            "AND(:minPrice IS NULL OR l.price >= :minPrice)")
+            "AND (:minPrice IS NULL OR l.price >= :minPrice)")
     List<Line> searchLines(@Param("carrierId") Long carrierId,
                            @Param("destination") String destination,
+                           @Param("departure") String departure,
                            @Param("minPrice") Integer minPrice,
                            @Param("maxPrice") Integer maxPrice, Pageable pageable);
 
