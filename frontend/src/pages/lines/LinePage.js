71aea893d53ed0baae45fb9b07e20300dc710df6
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-14 23:54:22 +0100

    Updated line page

--- frontend/src/pages/lines/LinePage.js
+++ frontend/src/pages/lines/LinePage.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
 import lineServiceAxios from "../../components/api/line.service.axios";
+import {
+  FormLabel,
+  InputGroup,
+  InputRow,
+  LinePageStyled,
+  SearchInput,
+  SearchParamsStyled,
+} from "./LinePage.styled";
 
 const LinePage = () => {
   const [lines, setLines] = useState(null);
@@ -23,9 +31,34 @@ const LinePage = () => {
   }, []);
 
   return (
-    <div>
-      <p>Lines</p>
-    </div>
+    <LinePageStyled>
+      <SearchParamsStyled>
+        <InputRow>
+          <InputGroup>
+            <FormLabel>Date:</FormLabel>
+            <SearchInput placeholder="Select date" type="datetime-local" />
+          </InputGroup>
+          <InputGroup>
+            <FormLabel>Destination:</FormLabel>
+            <SearchInput placeholder="Enter destination" />
+          </InputGroup>
+          <InputGroup>
+            <FormLabel>Departure:</FormLabel>
+            <SearchInput placeholder="Enter departure" />
+          </InputGroup>
+        </InputRow>
+        <InputRow>
+          <InputGroup>
+            <FormLabel>Min price:</FormLabel>
+            <SearchInput placeholder="Min price" />
+          </InputGroup>
+          <InputGroup>
+            <FormLabel>Max price:</FormLabel>
+            <SearchInput placeholder="Max price" />
+          </InputGroup>
+        </InputRow>
+      </SearchParamsStyled>
+    </LinePageStyled>
   );
 };
 
