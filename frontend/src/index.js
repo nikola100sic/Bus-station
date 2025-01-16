e0d67dd0d0e4254e03529170e24b5f10c6aafdae
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-16 19:19:55 +0100

    Implemented global styled

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./components/ui/GlobalStyle/GlobalStyled";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>
