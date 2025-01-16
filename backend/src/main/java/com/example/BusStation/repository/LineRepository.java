49bed07f70b165daa758c98b4eca6c8028f5c83c
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-16 11:34:45 +0100

    Updated line repository

--- backend/src/main/java/com/example/BusStation/repository/LineRepository.java
+++ backend/src/main/java/com/example/BusStation/repository/LineRepository.java
@@ -6,20 +6,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
 
     @Query("SELECT l FROM Line l JOIN l.carriers c " +
-            "WHERE (:carrierId IS NULL OR c.id = :carrierId) " +
+            "WHERE (:date IS NULL OR l.departureTime = :date) " +
             "AND (:destination IS NULL OR LOWER(l.destination) LIKE LOWER(CONCAT('%', :destination, '%'))) " +
             "AND (:departure IS NULL OR LOWER(l.departure) LIKE LOWER(CONCAT('%', :departure, '%'))) " +
             "AND (:maxPrice IS NULL OR l.price <= :maxPrice)" +
             "AND (:minPrice IS NULL OR l.price >= :minPrice)")
-    List<Line> searchLines(@Param("carrierId") Long carrierId,
+    List<Line> searchLines(@Param("date") LocalDateTime date,
                            @Param("destination") String destination,
                            @Param("departure") String departure,
+                           @Param("maxPrice") Integer maxPrice,
                            @Param("minPrice") Integer minPrice,
-                           @Param("maxPrice") Integer maxPrice, Pageable pageable);
+                           Pageable pageable);
 
 }
