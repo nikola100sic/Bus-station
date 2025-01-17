92ba326869cdc53199af5b7e2f14052b0ef11a33
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-17 12:39:53 +0100

    Added new test data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -6,6 +6,9 @@ INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_nu
 INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_number`) VALUES ('2', 'Njegoseva 28', 'nikolaTurs@gmail.com', 'Nikola turs', '018 345 443');
 INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_number`) VALUES ('3', 'Dositejeva 72', 'globus@gmail.com', 'Globus Travel', '011 673 233');
 INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_number`) VALUES ('4', 'Jovana Cvijica 2', 'nisexpres@gmail.com', 'Nis express', '018 145 233');
+INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_number`) VALUES ('5', ' Njegoseva 12', 'barbados@gmail.com', 'Barbados', '021 781 121');
+INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_number`) VALUES ('6', 'Kraljevica Marka 45', 'travelend@gmail.com', 'Travelend', '018 674 234');
+INSERT INTO `busstation`.`carrier` (`id`, `address`, `e_mail`, `name`, `phone_number`) VALUES ('7', 'Nikole Tesle 87', 'nntransport@gmail.com', 'Nn transport', '018 754 532');
 
 INSERT INTO `busstation`.`line` (`number_of_seats`, `price`, `departure_time`, `id`, `departure`, `destination`) VALUES ('33', '20', '2024-12-29 13:00', '1', 'Nis', 'Novi Sad');
 INSERT INTO `busstation`.`line` (`number_of_seats`, `price`, `departure_time`, `id`, `departure`, `destination`) VALUES ('33', '20', '2024-12-29 18:00', '2', 'Novi Sad', 'Nis');
