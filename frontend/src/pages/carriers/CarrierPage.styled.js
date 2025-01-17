425b0240e1e17932b94de87b8213033d338e7802
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-17 11:38:29 +0100

    Created styles for carriers page

---
+++ frontend/src/pages/carriers/CarrierPage.styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const CarrierPageStyled = styled.div`
+  display: flex;
+  flex: 1;
+  padding: 20px;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+export const CarrierCard = styled.div`
+  display: flex;
+  background-color: #ab993087;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  gap: 11px;
+  justify-content: center;
+  color: #ffffff;
+  padding: 20px;
+  border-radius: 28px;
+  font-family: ui-rounded;
+`;
