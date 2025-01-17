e1bc9d76f010f1e8d57376603621e332c371b69b
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-17 12:40:06 +0100

    Updated global styled

--- frontend/src/components/ui/GlobalStyle/GlobalStyled.js
+++ frontend/src/components/ui/GlobalStyle/GlobalStyled.js
@@ -1,28 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: 'Arial', sans-serif;
-    background-color: #f5f5f5;
-    overflow-x: hidden;
-  }
-
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-
   body::before {
     content: "";
     position: fixed;
-    top: 0;
-    left: 0;
     width: 100%;
     height: 100%;
     background-image: url(/bus.jpg);
