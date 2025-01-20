94a02ac7708b198b3b72df68ba1c4f31b4077e03
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-20 18:52:00 +0100

    Updated home page

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { GalleryContainer, HomePageStyle } from "./HomePage.styled";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  const handleGalleryClick = () => {
+    navigate("/gallery");
+  };
+
+  return (
+    <HomePageStyle>
+      <GalleryContainer
+        onClick={handleGalleryClick}
+        style={{ cursor: "pointer" }}
+      >
+        Gallery
+      </GalleryContainer>
+    </HomePageStyle>
+  );
 };
 
 export default Home;
