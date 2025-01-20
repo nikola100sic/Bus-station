87906f1bcfbdf8a6ecdf299203503d91844aae0d
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-20 18:52:10 +0100

    Updated styles for home page

---
+++ frontend/src/pages/home/HomePage.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HomePageStyle = styled.div`
+  display: flex;
+  flex: 1;
+  padding: 20px;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: flex-start;
+`;
+
+export const GalleryContainer = styled.div`
+  display: flex;
+  background-color: darkgoldenrod;
+  padding: 10px;
+`;
