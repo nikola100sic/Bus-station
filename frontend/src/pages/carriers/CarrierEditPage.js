f410f543f0692013b0d57c6a9cb65ec870c33047
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2025-01-21 22:34:17 +0100

    Updated carrier edit page

--- frontend/src/pages/carriers/CarrierEditPage.js
+++ frontend/src/pages/carriers/CarrierEditPage.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import carrierServiceAxios from "../../components/api/carrier.service.axios";
+import { CarrierFormContainer } from "../../components/shared/forms/Forms.styled";
 
 const CarrierEditPage = () => {
+  const routeParams = useParams();
+  const carrierId = routeParams.id;
+
+  const [carrier, setCarrier] = useState({
+    id: 0,
+    email: "",
+    address: "",
+    name: "",
+    phoneNumber: "",
+  });
+  const getCarrier = () => {
+    carrierServiceAxios
+      .getCarrier(carrierId)
+      .then((res) => {
+        setCarrier(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getCarrier();
+  }, []);
+
   return (
-    <div>
+    <CarrierFormContainer>
       <p>Edit page</p>
-    </div>
+    </CarrierFormContainer>
   );
 };
 
